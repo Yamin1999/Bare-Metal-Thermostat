ad6ba83bc73beaa3bbd129446671adce3c1820d5
Yamin1999 <yaminhaque1999@gmail.com>
2026-08-19 18:34:27 +0600

    Added EXTI13 and SysTick interrupts

--- drivers/systick.c
+++ drivers/systick.c
@@ -1,11 +1,18 @@
 #include "systick.h"
 #include "stm32f4xx.h" /* Already includes core_cm4.h (SysTick pointer) + SystemCoreClock */
 
+static volatile uint32_t tick_ms = 0;
+
+void SysTick_Handler(void)
+{
+    tick_ms++;
+}
+
 void systick_init(void)
 {
     SysTick->LOAD = (SystemCoreClock / 1000) - 1;
     SysTick->VAL  = 0;
-    SysTick->CTRL = (1 << 2) | (1 << 0); /* CLKSOURCE = CPU clock, ENABLE = 1 */
+    SysTick->CTRL = (1 << 2) | (1 << 1) | (1 << 0); /* CLKSOURCE, TICKINT, ENABLE */
 }
 
 void delay_ms(uint32_t ms)
@@ -14,4 +21,9 @@ void delay_ms(uint32_t ms)
     {
         while (!(SysTick->CTRL & (1 << 16))); /* wait for COUNTFLAG */
     }
+}
+
+uint32_t systick_get_ticks(void)
+{
+    return tick_ms;
 }
--- drivers/systick.h
+++ drivers/systick.h
@@ -5,5 +5,6 @@
 
 void systick_init(void);
 void delay_ms(uint32_t ms);
+uint32_t systick_get_ticks(void);
 
 #endif
